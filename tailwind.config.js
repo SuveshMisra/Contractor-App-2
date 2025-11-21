e9d5b8e0f6df8dc6568f75d87f691274bd04abe6
SuveshMisra <42189655+SuveshMisra@users.noreply.github.com>
2025-11-21 14:28:40 +0100

    Fixed dark mode error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // @ts-expect-error - nativewind/preset is not typed
+  darkMode: "class",
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
